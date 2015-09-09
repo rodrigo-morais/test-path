f079878286201b3e1f878fcc91eb32b1ae29ecd6
Rodrigo Morais <morais.rm@gmail.com>
2015-09-09 11:03:30 -0300

    Adding the solution of the problem.

--- Program.cs
+++ Program.cs
@@ -1,15 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace test_path
+public class Path
 {
-    class Program
+    private List<string> parts = new List<string>();
+
+    public string CurrentPath { get; private set; }
+
+    private void addPartToPath(string part)
     {
-        static void Main(string[] args)
+        this.parts.Add(part);
+    }
+
+    private void removeLastPartFromPath()
+    {
+        this.parts.RemoveAt(this.parts.Count - 1);
+    }
+
+    public Path(string path)
+    {
+        path = path.Replace("\"", "/");
+        path = path.Replace("\\", "/");
+        foreach (var part in path.Split('/'))
         {
+            this.addPartToPath(part);
         }
+        this.CurrentPath = path;
+    }
+
+    public Path Cd(string newPath)
+    {
+        newPath = newPath.Replace("\"", "/");
+        newPath = newPath.Replace("\\", "/");
+
+        foreach (var part in newPath.Split('/'))
+        {
+            if (part.Equals(".."))
+            {
+                this.removeLastPartFromPath();
+            }
+            else
+            {
+                this.addPartToPath(part);
+            }
+        }
+
+        this.CurrentPath = String.Join("/", this.parts.ToArray());
+
+        return this;
+    }
+
+    public static void Main(string[] args)
+    {
+        Path path = new Path("/a/b/c/d");
+        Console.WriteLine(path.Cd("../x").CurrentPath);
     }
 }
